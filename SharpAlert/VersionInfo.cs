b2b2cc6dcfeee4775f7ad252d8e029bfe5be4e5b
BunnyTub <gadiel047@gmail.com>
2026-05-12 16:07:10 -0400

    Update to release

--- SharpAlert/VersionInfo.cs
+++ SharpAlert/VersionInfo.cs
@@ -8,7 +8,7 @@ namespace SharpAlert
         // last build was 1891 before the upgrade to .NET 8
         public static int MajorVersion { get; } = 18;
         public static int MinorVersion { get; } = 4;
-        public static bool IsBetaVersion { get; } = true;
+        public static bool IsBetaVersion { get; } = false;
         public static string ShortFriendlyVersion
         {
             get
